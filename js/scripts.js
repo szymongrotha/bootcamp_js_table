0b23acf109f6741fd57b0bfa1170b0b0188f0548
Szymon Grotha <szymon.grotha@gmail.com>
2016-11-08 13:13:33 +0100

    Fiex source code issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-	var girlsNames = ['Anka', 'Paulina', 'Kasia', 'Ola'];
-	var boysNames = ['Szymon', 'Michał', 'Dawid', 'Stefan'];
-	var allNames = girlsNames.concat(boysNames);
-	var newName = "Mietek";
+var girlsNames = ['Anka', 'Paulina', 'Kasia', 'Ola'];
+var boysNames = ['Szymon', 'Michał', 'Dawid', 'Stefan'];
+var allNames = girlsNames.concat(boysNames);
+var newName = "Mietek";
 
-	if(allNames.indexOf(newName) === -1) {
-		var name = allNames.push(newName);
-	} else {
-		console.log('Imię Mietek już znajduje się w tablicy')
-	}
+if (allNames.indexOf(newName) === -1) {
+	var name = allNames.push(newName);
 
-	console.log(allNames)
+} else {
+	console.log('Imię ' + newName + ' znajduje się już w tablicy')
+}
+
+console.log(allNames)
